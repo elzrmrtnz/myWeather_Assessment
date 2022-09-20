986a77c9b6f541d89a1d028b3989e35287aa2d47
Eleazar Martinez <eleazar.martinez@accenture.com>
2022-09-20 18:19:27 +0800

    Update UI

---
+++ myWeather/Views/WeatherDay.swift
@@ -0,0 +1,48 @@
+//
+//  WeatherDay.swift
+//  myWeather
+//
+//  Created by eleazar.martinez on 9/20/22.
+//
+
+import SwiftUI
+//
+//struct WeatherDay: View {
+//    var day: String
+//    var temp: Int
+//    var icon: String
+//
+//    var body: some View {
+//        HStack {
+//            Text(day)
+//                .font(.title3)
+//
+//            Spacer()
+//
+//            URLImage(url: icon)
+//                .frame(width: 35, height: 35)
+//
+//            Spacer ()
+//
+//            HStack {
+//                Text(String(format: "Min: %.0f˚", myWeather.tempMin0))
+//                    .font(.title3)
+//                Text(String(format: "Max: %.0f˚", myWeather.tempMax0))
+//                    .font(.title3)
+//            }
+//
+//        }//hstack
+//    .padding()
+//    .frame(width: 350, height: 50)
+//    .foregroundColor(.white)
+//    .background(RoundedRectangle(cornerRadius: 10).fill(LinearGradient(gradient: Gradient(colors: [Color.gray.opacity(0.5), Color.gray]), startPoint: .top, endPoint: .bottom)).opacity(0.5))
+//    .shadow(color: Color.white.opacity(0.1), radius: 2, x: -2, y: -2)
+//    .shadow(color: Color.black.opacity(0.2), radius: 2, x: 2, y: 2)
+//    }
+//}
+//
+//struct WeatherDay_Previews: PreviewProvider {
+//    static var previews: some View {
+//        WeatherDay()
+//    }
+//}
